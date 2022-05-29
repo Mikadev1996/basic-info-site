142628c28aae92173d18fd8b75e5c7a5fa311c92
mika_laptop_windows <charmikadev@gmail.com>
2022-05-29 19:17:17 +0100

    navigates to page

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
+const myUrl = new URL("http://localhost:3000/");
 
 const server = http.createServer((req, res) => {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('Hello, world!');
-        res.end();
+
+        function handlePath(filePath) {
+                fs.readFile(filePath, (err, data) => {
+                        if (err) {
+                                console.log(err);
+                                return;
+                        }
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.write(data);
+                        res.end();
+                })
+        }
+        if (req.url === "/") {
+                handlePath('index.html');
+        }
+
+        else if (req.url === "/about") {
+                handlePath('about.html')
+        }
+
+        else if (req.url === "/contact-me") {
+                handlePath('contact-me.html');
+        }
+        else {
+                handlePath('404.html');
+        }
 });
+
 
 server.listen(port, () => {
         console.log(`Server running at ${port}`);
 });
+
