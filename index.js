55582fc8773f16573337e829e54a19661f83a2b0
mika_laptop_windows <charmikadev@gmail.com>
2022-06-02 17:45:55 +0100

    converted to express

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 const http = require('http');
 const fs = require('fs');
-
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = 3000;
-const myUrl = new URL("http://localhost:3000/");
 
-const server = http.createServer((req, res) => {
+app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, '/index.html'));
+})
 
-        function handlePath(filePath) {
-                fs.readFile(filePath, (err, data) => {
-                        if (err) {
-                                console.log(err);
-                                return;
-                        }
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.write(data);
-                        res.end();
-                })
-        }
-        if (req.url === "/") {
-                handlePath('index.html');
-        }
+app.get("/about", (req, res) => {
+        res.sendFile(path.join(__dirname, '/about.html'));
+})
 
-        else if (req.url === "/about") {
-                handlePath('about.html')
-        }
+app.get("/contact-me", (req, res) => {
+        res.sendFile(path.join(__dirname, '/contact-me.html'));
+})
 
-        else if (req.url === "/contact-me") {
-                handlePath('contact-me.html');
-        }
-        else {
-                handlePath('404.html');
-        }
-});
+app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, '/404.html'));
+})
+
+app.listen(port, () => {
+        console.log(`Example app running on ${port}`);
+})
 
 
-server.listen(port, () => {
-        console.log(`Server running at ${port}`);
-});
+//
+// function handlePath(filePath) {
+//         fs.readFile(filePath, (err, data) => {
+//                 if (err) {
+//                         console.log(err);
+//                         return;
+//                 }
+//                 res.writeHead(200, {'Content-Type': 'text/html'});
+//                 res.write(data);
+//                 res.end();
+//         })
+// }
+// if (req.url === "/") {
+//         handlePath('index.html');
+// }
+//
+// else if (req.url === "/about") {
+//         handlePath('about.html')
+// }
+//
+// else if (req.url === "/contact-me") {
+//         handlePath('contact-me.html');
+// }
+// else {
+//         handlePath('404.html');
+// }
 
